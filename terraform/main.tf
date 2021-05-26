4dc42c7377527a89e7ef157520a56894589eb651
Galen Guyer <galen@galenguyer.com>
2021-05-25 22:29:14 -0400

    change network subnet

--- terraform/main.tf
+++ terraform/main.tf
@@ -29,7 +29,7 @@ resource "azurerm_subnet" "kube-galenguyer-subnet" {
  name                 = "kube-galenguyer-subnet"
  resource_group_name  = azurerm_resource_group.kube.name
  virtual_network_name = azurerm_virtual_network.kube-galenguyer-vnet.name
- address_prefixes       = ["10.0.3.0/24"]
+ address_prefixes       = ["10.0.8.0/24"]
 }
 
 resource "azurerm_network_security_group" "kube-galenguyer-nsg" {
